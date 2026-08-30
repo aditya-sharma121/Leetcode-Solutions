e7cf91031764bded4f1f1ffbab662959899420b0
Aditya sharma <jaimaachaumukha@gmail.com>
2026-08-30 21:18:17 +0530

    Time: 0 ms (100%), Space: 43 MB (33.67%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDepth(String s) {
+      int ans=0,count=0;
+      for(int i=0;i<s.length();i++){
+        if(s.charAt(i)=='('){
+            count++;
+        }else if(s.charAt(i)==')'){
+            count--;
+        }
+      if(ans<count){
+        ans = count;
+      }  
+    }
+      return ans;
+    }
+}
