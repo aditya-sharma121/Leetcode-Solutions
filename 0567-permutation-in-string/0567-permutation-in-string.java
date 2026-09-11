dcb21134a190aa57f1c88ff847caf7c2a90b2258
Aditya sharma <jaimaachaumukha@gmail.com>
2026-09-11 13:47:22 +0530

    Time: 164 ms (10.34%), Space: 47 MB (14.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -8,15 +8,15 @@ class Solution {
     }
     return false;
 }
-        public boolean isAnagram(String s,String t){
-        int[] f1 = new int[26];
-        for(char ch : t.toCharArray()){
-            f1[ch-'a']++;
-        }
-        int[] f2 = new int[26];
-        for(char ch : s.toCharArray()){
-            f2[ch-'a']++;
-        }
+public boolean isAnagram(String s,String t){
+    int[] f1 = new int[26];
+    for(char ch : t.toCharArray()){
+        f1[ch-'a']++;
+    }
+    int[] f2 = new int[26];
+    for(char ch : s.toCharArray()){
+        f2[ch-'a']++;
+    }
         return Arrays.equals(f1,f2);
     }
 }
