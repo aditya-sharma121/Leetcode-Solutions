09a149f39919008e100f0ad206934c6a6066828c
Aditya sharma <jaimaachaumukha@gmail.com>
2026-09-17 19:50:35 +0530

    Time: 3 ms (52.06%), Space: 44.6 MB (56.88%) - LeetHub

---
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int secondsBetweenTimes(String startTime, String endTime) {
+        String[] start = startTime.split(":");
+        String[] end = endTime.split(":");
+        int startsec = Integer.parseInt(start[0]) * 3600 +
+         Integer.parseInt(start[1]) * 60 +  Integer.parseInt(start[2]);
+
+       int endsec = Integer.parseInt(end[0]) * 3600 +
+         Integer.parseInt(end[1]) * 60 +  Integer.parseInt(end[2]);
+
+
+      return Math.abs(endsec-startsec);
+    }
+}
